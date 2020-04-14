f08a0f100da3dde2d7699bada979453ef3ae700b
Joona Somerkivi <joona.somerkivi@nordcloud.com>
2020-04-14 15:38:24 +0300

    Initial commit

---
+++ src/init-terraform.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
+import argparse
+
+from tools import ptree
+
+
+def create_file(path, content = "", force = False, verbosity = 0):
+  if not os.path.exists(path) or force:
+      if verbosity > 0:
+          print(f"[+] {path}")
+      with open(path, 'w') as file_tf:
+          if force:
+            file_tf.write(content)
+  elif verbosity > 1:
+    print(f"[s] {path}")
+
+parser = argparse.ArgumentParser(description='Terraform project initializer, creates basic folder structure and placeholder files')
+parser.add_argument("project", help="Name of the project to be initialized", type=str)
+parser.add_argument(
+    "--target", help="Where to create the project to, defaults to current working directory", type=str, default=".")
+parser.add_argument(
+    "--module", "-m", help="Name(s) of the module(s) to create", type=str)
+parser.add_argument(
+    "--profile", "-p", help="Name(s) of the profile(s) to be initialized", type=str)
+parser.add_argument(
+    "--env", help="The environment(s) to create", default="sandbox", type=str)
+
+parser.add_argument(
+    "--awsprofile", "-a", help="The profile in your AWS configuration to use", type=str, default='default')
+parser.add_argument(
+    "--awsregion", help="AWS region, defaults to 'eu-west-1'", default="eu-west-1", type=str)
+
+
+parser.add_argument(
+    "--force", "-f", help="Force overwrite of any existing files", action="store_true"
+)
+parser.add_argument("-v", "--verbosity", action="count", default=0)
+
+
+args = parser.parse_args()
+modules = []
+profiles = []
+environments = []
+
+if args.target == '.':
+  cd = os.getcwd()
+else:
+  cd = args.target
+
+if args.module:
+  for item in args.module.split(","):
+    modules.append(item.strip())
+
+if args.profile:
+  for item in args.profile.split(","):
+    profiles.append(item.strip())
+
+if args.env:
+  for item in args.env.split(","):
+    environments.append(item.strip())
+
+
+if modules and args.verbosity > 0:
+  print(f'\nCreating {len(modules)} module(s)... ')
+
+for module in modules:
+  Path(f"{cd}/{args.project}/_modules/{module}").mkdir(parents=True, exist_ok=True)
+  for tf in ['base.tf', 'variables.tf']:
+    path = f"{cd}/{args.project}/_modules/{module}/{tf}"
+    create_file(path, force=args.force, verbosity=args.verbosity)
+
+  if args.verbosity > 1:
+    print(f'Module "{module}" created')
+
+if profiles and args.verbosity > 0:
+  print(f'\nCreating {len(profiles)} profile(s)...')
+
+for profile in profiles:
+  Path(f"{cd}/{args.project}/profiles/{profile}").mkdir(parents=True, exist_ok=True)
+  for tf in ['main.tf', 'variables.tf']:
+    path = f"{cd}/{args.project}/profiles/{profile}/{tf}"
+    create_file(path, force=args.force, verbosity=args.verbosity)
+  
+  create_file(
+      f"{cd}/{args.project}/profiles/{profile}/backend.tf",
+      force=args.force,
+      verbosity=args.verbosity,
+      content="""terraform {
+  backend "local" {
+  }
+  required_version = "~> 0.12.0"
+}"""
+  )
+
+  create_file(
+      f"{cd}/{args.project}/profiles/{profile}/provider.tf",
+      force=args.force,
+      verbosity=args.verbosity,
+      content=f"""provider "aws" {{
+  region    = "{args.awsregion}"
+  profile   = "{args.awsprofile}"
+  version   = "~> 2.39.0"
+}}"""
+  )
+  if args.verbosity > 1:
+    print(f'Profile "{profile}" created')
+
+    
+if environments and args.verbosity > 0:
+  print(f'\nCreating {len(environments)} environment(s)...')
+for env in environments:
+  Path(f"{cd}/{args.project}/environment/{env}").mkdir(parents=True, exist_ok=True)
+  path = f"{cd}/{args.project}/environment/{env}/input_vars.tfvars"
+  create_file(path, force=args.force, verbosity=args.verbosity, content=
+    f"""bucket = "<bucket-for-{args.project}-backend>"
+key = "alias/{args.project}-encryption-key"
+region = "{args.awsregion}"
+dynamodb_table = "{args.project}-backend"
+encrypt = "true"
+profile = "{args.awsprofile}"
+"""
+  )
+
+  if args.verbosity > 1:
+    print(f'Environment "{env}" created')
+
+if args.verbosity > 0:
+  print('\nDone; project structure:\n')
+  ptree(f'{cd}/{args.project}')
---
+++ src/tools.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+def ptree(dir_path):
+  print(dir_path.split('/')[-1])
+  for line in tree(Path(dir_path)):
+    print(line)
+
+
+def tree(dir_path: Path, prefix: str = ''):
+    # prefix components:
+    space = '    '
+    branch = '│   '
+    # pointers:
+    tee = '├── '
+    last = '└── '
+    """A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():  # extend the prefix and recurse:
+            extension = branch if pointer == tee else space
+            # i.e. space because last, └── , above so no more |
+            yield from tree(path, prefix=prefix+extension)
